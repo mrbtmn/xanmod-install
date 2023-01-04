2b84ca9d00cde6497afbe9857c5bc56ab68ea99c
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2023-01-04 13:28:38 +0800

    Update xanmod-install.sh

--- xanmod-install.sh
+++ xanmod-install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-install_headers=0
 
+# 需要安装的xanmod版本
+unset install_package
+
+# 需要安装的软件包
+unset install_packages
+
+unset x86_64_psabi_version
+
+xanmod_source_preinstalled=0
+
+#功能性函数：
 #定义几个颜色
 purple()                           #基佬紫
 {
@@ -29,12 +39,13 @@ blue()                             #蓝色
 #检查基本命令
 check_base_command()
 {
+    hash -r
     local i
-    local temp_command_list=('bash' 'true' 'false' 'exit' 'echo' 'test' 'sort' 'sed' 'awk' 'grep' 'cut' 'cd' 'rm' 'cp' 'mv' 'head' 'tail' 'uname' 'tr' 'md5sum' 'cat' 'find' 'type' 'command' 'wc' 'ls' 'mktemp' 'swapon' 'swapoff' 'mkswap' 'chmod' 'chown' 'export')
-    for i in ${!temp_command_list[@]}
+    local temp_command_list=('bash' 'sh' 'command' 'type' 'hash' 'install' 'true' 'false' 'exit' 'echo' 'test' 'sort' 'sed' 'awk' 'grep' 'cut' 'cd' 'rm' 'cp' 'mv' 'head' 'tail' 'uname' 'tr' 'md5sum' 'cat' 'find' 'wc' 'ls' 'mktemp' 'swapon' 'swapoff' 'mkswap' 'chmod' 'chown' 'chgrp' 'export' 'tar' 'gzip' 'mkdir' 'arch' 'uniq' 'dd' 'env')
+    for i in "${temp_command_list[@]}"
     do
-        if ! command -V "${temp_command_list[$i]}" > /dev/null; then
-            red "命令\"${temp_command_list[$i]}\"未找到"
+        if ! command -V "${i}" > /dev/null; then
+            red "命令\"${i}\"未找到"
             red "不是标准的Linux系统"
             exit 1
         fi
@@ -49,16 +60,16 @@ test_important_dependence_installed()
             temp_exit_code=0
         else
             red "安装依赖 \"$1\" 出错！"
-            green  "欢迎进行Bug report(https://github.com/kirin10000/Xray-script/issues)，感谢您的支持"
+            green  "欢迎进行Bug report(https://github.com/kirin10000/xanmod-install/issues)，感谢您的支持"
             yellow "按回车键继续或者Ctrl+c退出"
             read -s
         fi
-    elif apt -y --no-install-recommends install "$1"; then
+    elif $apt_no_install_recommends -y install "$1"; then
         temp_exit_code=0
     else
-        apt update
-        apt -y -f install
-        apt -y --no-install-recommends install "$1" && temp_exit_code=0
+        $apt update
+        $apt_no_install_recommends -y -f install
+        $apt_no_install_recommends -y install "$1" && temp_exit_code=0
     fi
     return $temp_exit_code
 }
@@ -82,19 +93,28 @@ ask_if()
     return 1
 }
 
-if [[ -d "/proc/vz" ]]; then
-    red "Error: Your VPS is based on OpenVZ, which is not supported."
-    exit 1
-fi
 check_base_command
-if [[ "$(type -P apt)" ]]; then
-    if [[ "$(type -P dnf)" ]] || [[ "$(type -P yum)" ]]; then
-        red "同时存在apt和yum/dnf"
+if [[ "$(type -P apt)" ]] || [ "$(type -P apt-get)" ]; then
+    if [[ "$(type -P dnf)" ]] || [[ "$(type -P microdnf)" ]] || [[ "$(type -P yum)" ]]; then
+        red "同时存在 apt/apt-get 和 dnf/microdnf/yum"
         red "不支持的系统！"
         exit 1
     fi
+    release="other-debian"
+    if [[ "$(type -P apt)" ]]; then
+        apt="apt"
+    else
+        apt="apt-get"
+    fi
+    apt_no_install_recommends="$apt --no-install-recommends"
 else
-    red "xanmod内核仅支持Debian系的系统，如Ubuntu,Debian,deepin,UOS"
+    red "apt,apt-get命令均不存在"
+    red "仅支持Debian基系统"
+    red "不支持的系统"
+    exit 1
+fi
+if [[ -z "${BASH_SOURCE[0]}" ]]; then
+    red "请以文件的形式运行脚本，或不支持的bash版本"
     exit 1
 fi
 if [ "$EUID" != "0" ]; then
@@ -102,26 +122,74 @@ if [ "$EUID" != "0" ]; then
     exit 1
 fi
 
-menu()
+check_x86-64_psapi()
+{
+    # https://dl.xanmod.org/check_x86-64_psabi.sh
+    # 一个脚本，来自xanmod官网
+cat > check_x86-64_psabi.sh << EOF
+#!/usr/bin/awk -f
+
+BEGIN {
+    while (!/flags/) if (getline < "/proc/cpuinfo" != 1) exit 1
+    if (/lm/&&/cmov/&&/cx8/&&/fpu/&&/fxsr/&&/mmx/&&/syscall/&&/sse2/) level = 1
+    if (level == 1 && /cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/) level = 2
+    if (level == 2 && /avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/) level = 3
+    if (level == 3 && /avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/) level = 4
+    if (level > 0) { print "CPU supports x86-64-v" level; exit level + 1 }
+    exit 1
+}    
+EOF
+    chmod +x check_x86-64_psabi.sh
+    ./check_x86-64_psabi.sh
+    local level=$?
+    if [ $level -le 1 ] || [ $level -gt 4 ]; then
+        red "获取x86-64扩展等级失败"
+        exit 1
+    fi
+    x86_64_psabi_version=$((level - 1))
+}
+
+get_install_package()
 {
     tyblue "===============安装xanmod内核==============="
     tyblue " 请选择你想安装的版本："
-    green  "   1.EDGE(推荐)"
-    green  "   2.STABLE(推荐)"
-    tyblue "   3.TT"
-    tyblue "   4.RT-EDGE"
-    tyblue "   5.RT"
-    tyblue "   6.LTS"
-    red    "   7.不安装"
+    green  "   1.MAIN(推荐)"
+    tyblue "   2.RT"
+    tyblue "   3.LTS"
+    tyblue "   0.不安装"
     echo
-    local choice=""
-    while [[ ! "$choice" =~ ^([1-9][0-9]*)$ ]] || ((choice>7))
+    local install_version
+    while true
     do
-        read -p "您的选择是：" choice
+        read -p "您的选择是：" install_version
+        if [[ ! "$install_version" =~ ^(0|[1-9][0-9]*)$ ]] || ((install_version>3)); then
+            continue
+        fi
+        if [ $install_version -eq 2 ] && [ $x86_64_psabi_version -eq 1 ]; then
+            red "你的CPU架构不支持RT内核，请选择MAIN/LTS"
+            sleep 2s
+            continue
+        fi
+        break
     done
-    [ $choice -eq 7 ] && exit 0
-    local xanmod_list=("-edge" "" "-tt" "-rt-edge" "-rt" "-lts")
-    install="linux-xanmod${xanmod_list[$((choice-1))]}"
+    [ $install_version -eq 0 ] && exit 0
+    if [ $install_version -eq 3 ]; then
+        install_package=linux-xanmod-lts
+    elif [ $install_version -eq 2 ]; then
+        if [ $x86_64_psabi_version -eq 2 ]; then
+            install_package=linux-xanmod-rt-x64v2
+        else
+            install_package=linux-xanmod-rt-x64v3
+        fi
+    else
+        if [ $x86_64_psabi_version -eq 1 ]; then
+            install_package=linux-xanmod-x64v1
+        elif [ $x86_64_psabi_version -eq 2 ]; then
+            install_package=linux-xanmod-x64v2
+        else
+            install_package=linux-xanmod-x64v3
+        fi
+    fi
 }
 
 check_mem()
@@ -134,75 +202,50 @@ check_mem()
     fi
 }
 
+install_xanmod_source()
+{
+    [[ -f '/etc/apt/sources.list.d/xanmod-kernel.list' ]] && xanmod_source_preinstalled=1
+    mkdir -p /etc/apt/sources.list.d
+    if ! curl -L https://dl.xanmod.org/gpg.key | apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add - || !  echo 'deb http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-kernel.list; then
+        red "添加源失败！"
+        exit 1
+    fi
+}
+
+restore_source()
+{
+    if [ $xanmod_source_preinstalled -ne 1 ]; then
+        rm '/etc/apt/sources.list.d/xanmod-kernel.list'
+    fi
+}
+
 remove_other_kernel()
 {
-    local temp_file
-    temp_file="$(mktemp)"
-    dpkg --list > "$temp_file"
-    local kernel_list_headers
-    kernel_list_headers=($(awk '{print $2}' "$temp_file" | grep '^linux-headers'))
-    local kernel_list_image
-    kernel_list_image=($(awk '{print $2}' "$temp_file" | grep '^linux-image'))
-    local kernel_list_modules
-    kernel_list_modules=($(awk '{print $2}' "$temp_file" | grep '^linux-modules'))
-    rm "$temp_file"
-    local i
-    local ok_install=0
-    for ((i=${#kernel_list_image[@]}-1;i>=0;i--))
+    kernel_packages=($(LANG="C.UTF-8" LANGUAGE="" dpkg --list | awk '{print $2}' | grep -E '^(linux-headers|linux-image|linux-modules)'))
+    for temp_package in "${install_packages[@]}"
     do
-        if [ "${kernel_list_image[$i]}" == "${install_image_list[0]}" ]; then
-            unset 'kernel_list_image[$i]'
-            ((ok_install++))
+        flag=0
+        for ((i=${#kernel_packages[@]}-1;i>=0;i--))
+        do
+            if [ "${kernel_packages[$i]}" == "$temp_package" ]; then
+                unset 'kernel_packages[$i]'
+                flag=1
+                break
+            fi
+        done
+        if [ $flag -eq 0 ]; then
+            red "错误:软件包 \"$temp_package\" 未发现，可能安装失败？"
+            green  "欢迎进行Bug report(https://github.com/kirin10000/xanmod-install/issues)，感谢您的支持"
+            red "卸载失败！"
+            return 1
         fi
     done
-    if [ "$ok_install" -lt "1" ] ; then
-        red "内核可能安装失败！不卸载"
-        return 1
-    fi
-    if [ ${#install_modules_list[@]} -eq 1 ]; then
-        ok_install=0
-        for ((i=${#kernel_list_modules[@]}-1;i>=0;i--))
-        do
-            if [ "${kernel_list_modules[$i]}" == "${install_modules_list[0]}" ]; then
-                unset 'kernel_list_modules[$i]'
-                ((ok_install++))
-            fi
-        done
-        if [ "$ok_install" -lt "1" ] ; then
-            red "内核可能安装失败！不卸载"
-            return 1
-        fi
-    fi
-    if [ $install_headers -eq 1 ]; then
-        ok_install=0
-        for ((i=${#kernel_list_headers[@]}-1;i>=0;i--))
-        do
-            if [ "${kernel_list_headers[$i]}" == "${install_headers_list[0]}" ]; then
-                unset 'kernel_list_headers[$i]'
-                ((ok_install++))
-            fi
-        done
-        if [ "$ok_install" -lt "1" ] ; then
-            red "内核可能安装失败！不卸载"
-            return 1
-        fi
-    fi
-    if [ ${#kernel_list_image[@]} -eq 0 ] && [ ${#kernel_list_modules[@]} -eq 0 ] && ([ $install_headers -eq 0 ] || [ ${#kernel_list_headers[@]} -eq 0 ]); then
-        red "未发现可卸载内核！不卸载"
-        return 1
-    fi
     yellow "卸载过程中如果询问YES/NO，请选择NO！"
     yellow "卸载过程中如果询问YES/NO，请选择NO！"
     yellow "卸载过程中如果询问YES/NO，请选择NO！"
     tyblue "按回车键以继续。。"
     read -s
-    local exit_code=1
-    if [ $install_headers -eq 1 ]; then
-        apt -y purge "${kernel_list_image[@]}" "${kernel_list_modules[@]}" "${kernel_list_headers[@]}" && exit_code=0
-    else
-        apt -y purge "${kernel_list_image[@]}" "${kernel_list_modules[@]}" && exit_code=0
-    fi
-    if [ $exit_code -eq 0 ]; then
+    if $apt -y --allow-change-held-packages purge "${kernel_packages[@]}"; then
         apt-mark manual "^grub"
         green "卸载完成"
     else
@@ -214,58 +257,53 @@ remove_other_kernel()
 
 main()
 {
-    menu
+    check_x86-64_psapi
+    get_install_package
     check_important_dependence_installed procps
     check_mem
-    check_important_dependence_installed gnupg1
-    check_important_dependence_installed wget
+    check_important_dependence_installed curl
     check_important_dependence_installed ca-certificates
-    check_important_dependence_installed initramfs-tools
-    local temp_xanmod_apt_source=0
-    [[ -f '/etc/apt/sources.list.d/xanmod-kernel.list' ]] && temp_xanmod_apt_source=1
-    echo 'deb http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-kernel.list
-    if ! wget -qO - https://dl.xanmod.org/gpg.key | apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -; then
-        red "添加源失败！"
+    check_important_dependence_installed gnupg1
+    install_xanmod_source
+    if ! test_important_dependence_installed initramfs-tools; then
+        red "依赖 initramfs-tools 安装失败！"
+        restore_source
         exit 1
     fi
-    if ! apt update; then
-        red "更新源失败！"
-        exit 1
-    fi
-    local temp_list
-    temp_list=($(LANG="en_US.UTF-8" LANGUAGE="en_US:en" apt-cache depends "$install" | grep -i "Depends:" | awk '{print $2}'))
-    local i
-    for i in ${!temp_list[@]}
+    $apt update
+    local install_image_list
+    local install_modules_list
+    local install_headers_list
+    local temp_package
+    local temp_packages=($(LANG="en_US.UTF-8" LANGUAGE="en_US:en" apt-cache depends "$install_package" | grep -i "Depends:" | awk '{print $2}'))
+    for temp_package in "${temp_packages[@]}"
     do
-        if [[ "${temp_list[$i]}" =~ ^linux-headers-.*-xanmod ]]; then
-            install_headers_list+=("${temp_list[$i]}")
-        elif [[ "${temp_list[$i]}" =~ ^linux-image-.*-xanmod ]]; then
-            install_image_list+=("${temp_list[$i]}")
-        elif [[ "${temp_list[$i]}" =~ ^linux-modules-.*-xanmod ]]; then
-            install_modules_list+=("${temp_list[$i]}")
+        if [[ "${temp_package}" =~ ^linux-headers-.*xanmod.* ]]; then
+            install_headers_list+=("${temp_package}")
+            tyblue "info: add package \"${temp_package}\""
+        elif [[ "${temp_package}" =~ ^linux-image-.*xanmod.* ]]; then
+            install_image_list+=("${temp_package}")
+            tyblue "info: add package \"${temp_package}\""
+        elif [[ "${temp_package}" =~ ^linux-modules-.*xanmod.* ]]; then
+            install_modules_list+=("${temp_package}")
+            tyblue "info: add package \"${temp_package}\""
         fi
     done
-    if [ ${#install_image_list[@]} -ne 1 ] || [ ${#install_modules_list[@]} -gt 1 ] || ([ $install_headers -eq 1 ] && [ ${#install_headers_list[@]} -ne 1 ]); then
-        red "获取版本异常"
+    if [ ${#install_image_list[@]} -ne 1 ] || [ ${#install_modules_list[@]} -gt 1 ] || [ ${#install_headers_list[@]} -ne 1 ]; then
+        red "获取需安装软件包名失败"
+        restore_source
         exit 1
     fi
-    local exit_code=1
-    if [ $install_headers -eq 0 ]; then
-        apt -y --no-install-recommends install "${install_image_list[@]}" "${install_modules_list[@]}" && exit_code=0
-    else
-        apt -y --no-install-recommends install "${install_image_list[@]}" "${install_modules_list[@]}" "${install_headers_list[@]}" && exit_code=0
-    fi
-    [ $exit_code -ne 0 ] && apt -y -f install
-    if [ $temp_xanmod_apt_source -eq 0 ]; then
-        rm /etc/apt/sources.list.d/xanmod-kernel.list
-        apt update
-    fi
-    if [ $exit_code -ne 0 ]; then
-        red "安装失败！"
+    install_packages=("${install_image_list[@]}" "${install_modules_list[@]}" "${install_headers_list[@]}")
+    if ! $apt_no_install_recommends -y install "${install_packages[@]}"; then
+        $apt -y -f install
+        red "内核安装失败！"
+        restore_source
         exit 1
     fi
+    ask_if "是否删除其他内核？(y/n)" && remove_other_kernel
+    restore_source
     green "安装完成"
-    ask_if "是否卸载其它内核？(y/n)" && remove_other_kernel
     yellow "系统需要重启"
     if ask_if "现在重启系统? (y/n)"; then
         reboot
